abdc64f1fffd8daacdc282c382e0d33d6ea58bb5
Taner <97017072+justtanner12@users.noreply.github.com>
2023-09-18 14:53:24 -0500

    first commit made

---
+++ src/Week3Assignment.java
@@ -0,0 +1,11 @@
+
+public class Week3Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double value1  = 7.00;
+double value2 = 9.99;
+System.out.println(value1 + value2);
+	}
+
+}
